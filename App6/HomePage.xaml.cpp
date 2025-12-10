50732c85699ac5ce06890bc212fbce0d3609ac3b
Administrator <Administrator>
2025-12-10 15:24:28 +0800

    prepare use new settings

--- App6/HomePage.xaml.cpp
+++ App6/HomePage.xaml.cpp
@@ -17,7 +17,7 @@ namespace winrt::App6::implementation
 {
     void HomePage::Button_Click(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
     {
-        exit(0);
+	    exit(0);
     }
 
     void HomePage::Button_Click_Game(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
